2e7d72c0f9e78dc3de3513ea9abb1087de71d84b
zhihanliu <zhihanliu@hotmail.com>
2018-04-26 16:27:23 -0500

    first try

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,2 +1,8 @@
 console.log("Hello World!");
-console.log("Hello World!");
+
+const http  = require("http");
+
+http.createServer((req, res)=>{
+
+res.end("Hello Node Js");
+}).listen(3000,"localhost");
